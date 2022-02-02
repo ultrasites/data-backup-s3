98ab50512ea3ea7a8e26ae8460c1f6c18b77bfa6
Manuel Dierkes <m.dierkes@ultra-sites.de>
2022-02-02 13:43:17 +0100

    init commit 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,111 @@
 # data-backup-s3
-Data Backup to S4
+
+Data file backup to S3 instances!
+
+## Overview
+
+This is the easiest way to backup your files to s3 based von python running in a docker container.
+
+The following features are available:
+
+- backup data with the given volume
+- automatic upload to S3 (e.g. [MinIO](https://min.io), AWS, ...)
+- connect to any container running on the same system
+- select how often to run a backup
+- select when to start the first backup, whether time of day or relative to container start time
+
+## Backup
+
+To run a backup, launch <code>data-backup-s3</code> image as a container with the correct parameters. Everything is controlled by environment variables passed to the container.
+
+For example:
+
+<code>docker run -d --restart=always -e BACKUP_FREQ=60 -e BACKUP_BEGIN=2330 -e S3_TARGET=s3://... -v /local/file/path:/home/backupuser/data ultrasites/data-backup-s3</code>
+
+The above will run a backup every 60 minutes, beginning at the next 2330 local time.
+
+### Environment
+
+- <code>BACKUP_FREQ</code>: How often to do a BACKUP, in minutes. Defaults to 1440 minutes, or once per day.
+- <code>BACKUP_BEGIN</code>: What time to do the first BACKUP. Defaults to immediate. Must be in one of two formats:
+  - Absolute: HHMM, e.g. 2330 or 0415
+  - Relative: +MM, i.e. how many minutes after starting the container, e.g. +0 (immediate), +10 (in 10 minutes), or +90 in an hour and a half
+- <code>BACKUP_CRON</code>: Set the BACKUP schedule using standard crontab syntax, a single line.
+- <code>RUN_ONCE</code>: Run the backup once and exit if RUN_ONCE is set. Useful if you use an external scheduler (e.g. as part of an orchestration solution like Cattle or Docker Swarm or kubernetes cron jobs) and don't want the container to do the scheduling internally. If you use this option, all other scheduling options, like BACKUP_FREQ and BACKUP_BEGIN and BACKUP_CRON, become obsolete.
+- <code>BACKUP_DEBUG</code>: If set to true, print copious shell script messages to the container log. Otherwise only basic messages are printed.
+- <code>BACKUP_TARGET</code>:
+  S3 target with the format s3://bucketname/path. Connection via awscli.
+- <code>AWS_ACCESS_KEY_ID</code>: AWS Key ID
+- <code>AWS_SECRET_ACCESS_KEY</code>: AWS Secret Access Key
+- <code>AWS_DEFAULT_REGION</code>: Region in which the bucket resides
+- <code>AWS_ENDPOINT_URL</code>: Specify an alternative endpoint for s3 interopable systems e.g. Digitalocean
+- <code>AWS_CLI_OPTS</code>: Additional arguments to be passed to the aws part of the aws s3 cp command, click here for a list. Be careful, as you can break something!
+- <code>AWS_CLI_S3_CP_OPTS</code>: Additional arguments to be passed to the s3 cp part of the aws s3 cp command, click here for a list. If you are using AWS KMS, sse, sse-kms-key-id, etc., may be of interest.
+- <code>COMPRESSION</code>: Compression to use. Supported are: gzip (default), bzip2
+- <code>TMP_PATH</code>: tmp directory to be used during backup creation and other operations. Optional, defaults to /tmp
+
+### Scheduling
+
+There are several options for scheduling how often a backup should run:
+
+- <code>RUN_ONCE</code>: run just once and exit.
+- <code>BACKUP_FREQ</code> and <code>BACKUP_BEGIN</code>: run every x minutes, and run the first one at a particular time.
+- <code>BACKUP_CRON</code>: run on a schedule.
+  Cron Scheduling
+  If a cron-scheduled backup takes longer than the beginning of the next backup window, it will be skipped. For example, if your cron line is scheduled to backup every hour, as follows:
+
+<code>0 \* \* \* \*</code>
+And the backup that runs at 13:00 finishes at 14:05, the next backup will not be immediate, but rather at 15:00.
+
+The cron algorithm is as follows: after each backup run, calculate the next time that the cron statement will be true and schedule the backup then.
+
+#### Order of Priority
+
+The scheduling options have an order of priority:
+
+<code>RUN_ONCE</code> runs once, immediately, and exits, ignoring everything else.
+<code>BACKUP_CRON</code>: runs according to the cron schedule, ignoring BACKUP_FREQ and BACKUP_BEGIN.
+<code>BACKUP_FREQ</code> and <code>BACKUP_BEGIN</code>: if nothing else is set.
+
+### Permissions
+
+By default, the backup/restore process does not run as root (UID O). Whenever possible, you should run processes (not just in containers) as users other than root. In this case, it runs as username appuser with UID/GID 1005.
+
+In most scenarios, this will not affect your backup process negatively. However, if you are using the "Local" BACKUP target, i.e. your <code>BACKUP_TARGET</code> starts with / - and, most likely, is a volume mounted into the container - you can run into permissions issues. For example, if your mounted directory is owned by root on the host, then the backup process will be unable to write to it.
+
+In this case, you have two options:
+
+Run the container as root, docker run --user 0 ... or, in docker-compose.yml, user: "0"
+Ensure your mounted directory is writable as UID or GID 1005.
+
+### Forrmat
+
+The BACKUP target is where you want the backup files to be saved. The backup file always is a compressed file the following format:
+
+_data_backup_YYYY-MM-DDTHH:mm:ssZ.<code>compression</code>_
+
+Where the date is RFC3339 date format, excluding the milliseconds portion.
+
+YYYY = year in 4 digits
+MM = month number from 01-12
+DD = date for 01-31
+HH = hour from 00-23
+mm = minute from 00-59
+ss = seconds from 00-59
+T = literal character T, indicating the separation between date and time portions
+Z = literal character Z, indicating that the time provided is UTC, or "Zulu"
+compression = appropriate file ending for selected compression, one of: gz (gzip, default); bz2 (bzip2)
+The time used is UTC time at the moment the BACKUP begins.
+
+If you use a URL like s3://bucket/path, you can have it save to an S3 bucket.
+
+Note that for s3, you'll need to specify your AWS credentials and default AWS region via <code>AWS_ACCESS_KEY_ID</code>, <code>AWS_SECRET_ACCESS_KEY</code> and <code>AWS_DEFAULT_REGION</code>
+
+## About us
+
+![](https://www.ultra-sites.de/wp-content/uploads/2022/02/logo_ultrasites.png)
+&copy; 2022 Ultra Sites Medienagentur
+
+https://www.ultra-sites.de
+
+Visit us on [Github](https://github.com/ultrasites)!
---
+++ functions.sh
@@ -0,0 +1,381 @@
+#!/bin/bash
+# Function definitions used in the entrypoint file.
+
+#
+# Environment variable reading function
+#
+# The function enables reading environment variable from file.
+#
+function file_env() {
+  local var="$1"
+  local fileVar="${var}_FILE"
+  local def="${2:-}"
+  if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+    echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+    exit 1
+  fi
+  local val="$def"
+  if [ "${!var:-}" ]; then
+    val="${!var}"
+  elif [ "${!fileVar:-}" ]; then
+    val="$(< "${!fileVar}")"
+  fi
+  export "$var"="$val"
+  unset "$fileVar"
+}
+
+
+#
+# URI parsing function
+#
+# The function creates global variables with the parsed results.
+# It returns 0 if parsing was successful or non-zero otherwise.
+#
+# [schema://][user[:password]@]host[:port][/path][?[arg1=val1]...][#fragment]
+#
+function uri_parser() {
+  uri=()
+  # uri capture
+  full="$@"
+
+    # safe escaping
+    full="${full//\`/%60}"
+    full="${full//\"/%22}"
+
+		# URL that begins with '/' is like 'file:///'
+		if [[ "${full:0:1}" == "/" ]]; then
+			full="file://localhost${full}"
+		fi
+		# file:/// should be file://localhost/
+		if [[ "${full:0:8}" == "file:///" ]]; then
+			full="${full/file:\/\/\//file://localhost/}"
+		fi
+
+    # top level parsing
+    pattern='^(([a-z0-9]{2,5})://)?((([^:\/]+)(:([^@\/]*))?@)?([^:\/?]+)(:([0-9]+))?)(\/[^?]*)?(\?[^#]*)?(#.*)?$'
+    [[ "$full" =~ $pattern ]] || return 1;
+
+    # component extraction
+    full=${BASH_REMATCH[0]}
+		uri[uri]="$full"
+    uri[schema]=${BASH_REMATCH[2]}
+    uri[address]=${BASH_REMATCH[3]}
+    uri[user]=${BASH_REMATCH[5]}
+    uri[password]=${BASH_REMATCH[7]}
+    uri[host]=${BASH_REMATCH[8]}
+    uri[port]=${BASH_REMATCH[10]}
+    uri[path]=${BASH_REMATCH[11]}
+    uri[query]=${BASH_REMATCH[12]}
+    uri[fragment]=${BASH_REMATCH[13]}
+
+		# does the user have a domain?
+		if [[ -n ${uri[user]} && ${uri[user]} =~ ^([^\;]+)\;(.+)$ ]]; then
+			uri[userdomain]=${BASH_REMATCH[1]}
+			uri[user]=${BASH_REMATCH[2]}
+		fi
+		return 0
+}
+
+
+
+#
+# execute data backup
+#
+function do_backup() {
+  now=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+
+  SOURCE=data_backup_${now}.$EXTENSION
+
+  # do the backup
+  workdir="${TMP_PATH}/backup.$$"
+  rm -rf $workdir
+  mkdir -p $workdir
+
+  # copy volume data to workdir
+  cp -R  $BACKUP_DATA $workdir
+
+  # compress
+  tar -C $workdir -cvf - . | $COMPRESS > ${TMPDIR}/${SOURCE}
+  [ $? -ne 0 ] && return 1
+  rm -rf $workdir
+  [ $? -ne 0 ] && return 1
+
+  return 0
+}
+
+#
+# place the backup to s3
+#
+function save_backup_to_s3() {
+  local target=$1
+  uri_parser ${target}
+  [[ -n "$AWS_ENDPOINT_URL" ]] && AWS_ENDPOINT_OPT="--endpoint-url $AWS_ENDPOINT_URL"
+  aws ${AWS_CLI_OPTS} ${AWS_ENDPOINT_OPT} s3 cp ${TMPDIR}/${SOURCE} "${S3_TARGET}/${TARGET}"
+  [ $? -ne 0 ] && return 1
+  return 0
+}
+
+#
+# calculate seconds until next cron match
+#
+function wait_for_cron() {
+  local cron="$1"
+  local compare="$2"
+  local last_run="$3"
+  # we keep a copy of the actual compare time, because we might shift the compare time in a moment
+  local comparesec=$compare
+  # there must be at least 60 seconds between last run and next run, so if it is less than 60 seconds,
+  #   add differential seconds to $compare
+  local compareDiff=$(($compare - $last_run))
+  if [ $compareDiff -lt 60 ]; then
+    compare=$(($compare + $(( 60-$compareDiff )) ))
+  fi
+
+  # cron only works in minutes, so we want to round down to the current minute
+  # e.g. if we are at 20:06:25, we need to treat it as 20:06:00, or else our waittime will be -25
+  # on the other hand, if we are at 20:06:00, do not round it down
+  local current_seconds=$(date --date="@$comparesec" +"%-S")
+  if [ $current_seconds -ne 0 ]; then
+    comparesec=$(( $comparesec - $current_seconds ))
+  fi
+
+  # reminder, cron format is:
+  # minute(0-59)
+  #   hour(0-23)
+  #     day of month(1-31)
+  #       month(1-12)
+  #         day of week(0-6 = Sunday-Saturday)
+  local cron_minute=$(echo -n "$cron" | awk '{print $1}')
+  local cron_hour=$(echo -n "$cron" | awk '{print $2}')
+  local cron_dom=$(echo -n "$cron" | awk '{print $3}')
+  local cron_month=$(echo -n "$cron" | awk '{print $4}')
+  local cron_dow=$(echo -n "$cron" | awk '{print $5}')
+
+  local success=1
+
+  # when is the next time we hit that month?
+  local next_minute=$(date --date="@$compare" +"%-M")
+  local next_hour=$(date --date="@$compare" +"%-H")
+  local next_dom=$(date --date="@$compare" +"%-d")
+  local next_month=$(date --date="@$compare" +"%-m")
+  local next_dow=$(date --date="@$compare" +"%-u")
+  local next_year=$(date --date="@$compare" +"%-Y")
+
+  # date returns DOW as 1-7/Mon-Sun, we need 0-6/Sun-Sat
+  next_dow=$(( $next_dow % 7 ))
+
+  local cron_next=
+
+  # logic for determining next time to run
+  # start by assuming our current min/hr/dom/month/dow is good, store it as "next"
+  # go through each section: if it matches, keep going; if it does not, make it match or move ahead
+
+  while [ "$success" != "0" ]; do
+    # minute:
+    # if minute matches, move to next step
+    # if minute does not match, move "next" minute to the time that does match in cron
+    #   if "next" minute is ahead of cron minute, then increment "next" hour by one
+    #   move to hour
+    cron_next=$(next_cron_expression "$cron_minute" 59 "$next_minute")
+    if [ "$cron_next" != "$next_minute" ]; then
+      if [ "$next_minute" -gt "$cron_next" ]; then
+        next_hour=$(( $next_hour + 1 ))
+      fi
+      next_minute=$cron_next
+    fi
+
+    # hour:
+    # if hour matches, move to next step
+    # if hour does not match:
+    #   if "next" hour is ahead of cron hour, then increment "next" day by one
+    #   set "next" hour to cron hour, set "next" minute to 0, return to beginning of loop
+    cron_next=$(next_cron_expression "$cron_hour" 23 "$next_hour")
+    if [ "$cron_next" != "$next_hour" ]; then
+      if [ "$next_hour" -gt "$cron_next" ]; then
+        next_dom=$(( $next_dom + 1 ))
+      fi
+      next_hour=$cron_next
+      next_minute=0
+    fi
+
+    # weekday:
+    # if weekday matches, move to next step
+    # if weekday does not match:
+    #   move "next" weekday to next matching weekday, accounting for overflow at end of week
+    #   reset "next" hour to 0, reset "next" minute to 0, return to beginning of loop
+    cron_next=$(next_cron_expression "$cron_dow" 6 "$next_dow")
+    if [ "$cron_next" != "$next_dow" ]; then
+      dowDiff=$(( $cron_next - $next_dow ))
+      if [ "$dowDiff" -lt "0" ]; then
+        dowDiff=$(( $dowDiff + 7 ))
+      fi
+      next_dom=$(( $next_dom + $dowDiff ))
+      next_hour=0
+      next_minute=0
+    fi
+
+    # dom:
+    # if dom matches, move to next step
+    # if dom does not match:
+    #   if "next" dom is ahead of cron dom OR "next" month does not have crom dom (e.g. crom dom = 30 in Feb),
+    #       increment "next" month, reset "next" day to 1, reset "next" minute to 0, reset "next" hour to 0, return to beginning of loop
+    #   else set "next" day to cron day, reset "next" minute to 0, reset "next" hour to 0, return to beginning of loop
+    maxDom=$(max_day_in_month $next_month $next_year)
+    cron_next=$(next_cron_expression "$cron_dom" 30 "$next_dom")
+    if [ "$cron_next" != "$next_dom" ]; then
+      next_hour=0
+      next_minute=0
+    fi
+    if [ $next_dom -gt $cron_next -o $next_dom -gt $maxDom ]; then
+      next_month=$(( $next_month + 1 ))
+      if [ $next_month -gt 12 ]; then
+        next_month=$(( $next_month - 12))
+        next_year=$(( $next_year + 1 ))
+      fi
+      next_dom=1
+    else
+      next_dom=$cron_next
+    fi
+
+
+    # month:
+    # if month matches, move to next step
+    # if month does not match:
+    #   if "next" month is ahead of cron month, increment "next" year by 1
+    #   set "next" month to cron month, set "next" day to 1, set "next" minute to 0, set "next" hour to 0
+    #   return to beginning of loop
+    cron_next=$(next_cron_expression "$cron_month" 12 "$next_month")
+    if [ "$cron_next" != "$next_month" ]; then
+      # must be sure to roll month if needed
+      if [ $cron_next -gt 12 ]; then
+        next_year=$(( $next_year + 1 ))
+        cron_next=$(( $cron_next - 12 ))
+      fi
+      if [ $next_month -gt $cron_next ]; then
+        next_year=$(( $next_year + 1 ))
+      fi
+      next_month=$cron_next
+      next_day=1
+      next_minute=0
+      next_hour=0
+    fi
+
+    success=0
+  done
+  # success: "next" is now set to the next match!
+
+  local future=$(date --date="${next_year}-${next_month}-${next_dom}T${next_hour}:${next_minute}:00" +"%s")
+  local futurediff=$(($future - $comparesec))
+  echo $futurediff
+}
+
+# next_cron_expression function that takes a cron term, e.g. "3", "4-7", "*", "3,4-7", "*/5", "3-25/5",
+# and calculates the lowest term that fits the cron expression that is equal to or greater than some number.
+# uses the "max" argument to determine the maximum
+# For example, given the arguments, these are the results and why:
+# "*" "60" "4"       -> "4"   4 is the number that is greater than or equal to  "*"
+# "4" "60" "4"       -> "4"   4 is the number that is greater than or equal to  "4"
+# "5" "60" "4"       -> "5"   5 is the next number that matches "5", and is >= 4
+# "3-7" "60" "4"     -> "4"   4 is the number that fits within 3-7
+# "3-7" "60" "9"     -> "3"    no number in the range 3-7 ever is >= 9, so next one will be 3 when we circle back
+# "*/2" "60" "4"     -> "4"   4 is divisible by 2
+# "*/5" "60" "4"     -> "5"   5 is the next number in the range * that is divisible by 5, and is >= 4
+# "0-20/5" "60" "4"  -> "5"   5 is the next number in the range 0-20 that is divisible by 5, and is >= 4
+# "15-30/5" "60" "4" -> "15"  15 is the next number in the range 15-30 that is in increments of 5, and is >= 4
+# "15-30/5" "60" "20"-> "20"  20 is the next number in the range 15-30 that is in increments of 5, and is >= 20
+# "15-30/5" "60" "35"-> "15"    no number in the range 15-30/5 will ever be >=35, so 15 is the first circle back
+# "*/10" "12" "11"   -> "0"    the next match after 11 would be 20, but that would be greater than the maximum, so we circle back to 0
+#
+function next_cron_expression() {
+  local crex="$1"
+  local max="$2"
+  local num="$3"
+
+  # expand the list - note that this can handle a single-element list
+  local allvalid=""
+  local tmpvalid=""
+  # take each comma-separated expression
+  local parts=${crex//,/ }
+  # replace * with # so that we can handle * as one of comma-separated terms without doing shell expansion
+  parts=${parts//\*/#}
+  for i in $parts; do
+    # if it is a * or exact match, just add the number
+    if [ "$i" = "#" -o "$i" = "$num" ]; then
+      echo $num
+      return 0
+    fi
+
+    # it might be a step function, so we will have to reduce from the total range
+    partstep=${i##*\/}
+    partnum=${i%%\/*}
+    tmpvalid=""
+    local start=
+    local end=
+    if [ "${partnum}" = "#" ]; then
+      # calculate all of the numbers until the max
+      start=0
+      end=$max
+    else
+      # handle a range like 3-7, which includes a single number like 4
+      start=${partnum%%-*}
+      end=${partnum##*-}
+    fi
+    # calculate the valid ones just for this range
+    tmpvalid=$(seq $start $end)
+
+    # it is a step function if the partstep is not the same as the whole thing
+    if [ "$partstep" != "$i" ]; then
+      # add to allvalid only the ones that match the term
+      # there are two possible use cases:
+      # first number is 0: any divisible by the partstep, i.e. j%partstep
+      # first number is not 0: start at first and increment by partstep until we run out
+      #    this latter one is just the equivalent of dropping all numbers by (first) and then seeing if divisible
+      for j in $tmpvalid; do
+        if [ $(( (${j} - ${start}) % ${partstep} )) -eq 0 ]; then
+          allvalid="$allvalid $j"
+        fi
+      done
+    else
+      # if it is not a step function, just add the tmpvalid to the allvalid
+      allvalid="$allvalid $tmpvalid"
+    fi 
+  done
+
+  # sort for deduplication and ordering
+  allvalid=$(echo $allvalid | tr ' ' '\n' | sort -n -u | tr '\n' ' ')
+  for i in $allvalid; do
+    if [ "$i" -ge "$num" ]; then
+      echo $i
+      return 0
+    fi
+  done
+  # if we got here, no number matched, so take the very first one
+  echo ${allvalid%% *}
+}
+
+function max_day_in_month() {
+  local month="$1"
+  local year="$1"
+
+  case $month in
+    "1"|"3"|"5"|"7"|"8"|"10"|"12")
+      echo 31
+      ;;
+    "2")
+      local div4=$(( $year % 4 ))
+      local div100=$(( $year % 100 ))
+      local div400=$(( $year % 400 ))
+      local days=28
+      if [ "$div4" = "0" -a "$div100" != "0" ]; then
+        days=29
+      fi
+      if [ "$div400" = "0" ]; then
+        days=29
+      fi
+      echo $days
+      ;;
+    *)
+      echo 30
+      ;;
+  esac
+}
